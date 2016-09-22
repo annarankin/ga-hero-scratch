4b1470b5ecce1f67a6aededfc5daba8c6202911d
Anna Rankin <anna@annarankin.com>
2016-09-22 17:29:20 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var config = require('./config.js')
+
+var PORT = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var request = require('request');
+var engine = require('ejs-locals')
+
+app.engine('ejs', engine)
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  request({ url: `https://${config.jira_hostname}/rest/api/2/search?jql=labels=ga-hero+AND+assignee=Empty`,
+            headers: {
+              'Authorization': config.auth
+            },
+            encoding: 'utf8'
+          }, (err, response, body) => {
+            data = JSON.parse(body)
+            console.log(body)
+            res.render('index', {issues: data.issues})
+          })
+})
+
+app.listen(PORT, () => {
+  console.log(`Alive and listening on port ${PORT}`)
+});
+
